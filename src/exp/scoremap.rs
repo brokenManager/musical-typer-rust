9a2c9111e43a7455ea3735e1320746b42d45ef19
MikuroXina <ryosukadnak@gmail.com>
2020-03-30 21:18:28 +0900

    Impl parsing property

--- src/exp/scoremap.rs
+++ src/exp/scoremap.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use regex::Regex;
 
 use super::note::Note;
@@ -6,24 +8,68 @@ use super::note::Note;
 pub enum ScoremapError {
   UnexceptedEndOfFile,
   InvalidCommand { line_num: u64, reason: &'static str },
+  InvalidPropertyDeifinition { line_num: u64, reason: &'static str },
   InvalidStatementDeifinition { line_num: u64, reason: &'static str },
 }
 
-pub struct ScoremapMetadata {}
+const METADATA_KEYS: &[&'static str] = &[
+  "title",
+  "song_author",
+  "singer",
+  "score_author",
+  "song_data",
+  "bpm",
+];
+
+pub type ScoremapMetadata = HashMap<String, String>;
 
 pub struct Scoremap {
   metadata: ScoremapMetadata,
   notes: Vec<Note>,
 }
 
+const PROPERTY: &str =
+  r"^:([[:^space:]]+)[[:space:]]+([[:^space:]]+)$";
 const COMMENT: &str = r"^[[:space:]]*#?.*$";
 const COMMAND: &str =
   r"^[[:space:]]*\[[[:space:]]*(.*)[[:space:]]*\][[:space:]]*$";
 const HIRAGANA: &str = r"^:([あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわゐゑをんぁぃぅぇぉゃゅょゎっーがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽ]+)$";
 const CAPTION: &str = r"^>>(.+)$";
 
+fn captures_vec<'a>(this: &'a Regex, text: &'a str) -> Vec<&'a str> {
+  this
+    .captures(text)
+    .unwrap()
+    .iter()
+    .skip(1)
+    .map(|capture| capture.unwrap().as_str())
+    .collect::<Vec<_>>()
+}
+
 #[test]
 fn pattern_tests() {
+  let reg = Regex::new(PROPERTY).unwrap();
+  assert_eq!(
+    captures_vec(&reg, ":title        キミのチカラ"),
+    vec!["title", "キミのチカラ"]
+  );
+  assert_eq!(
+    captures_vec(&reg, ":song_author  佐々木英州"),
+    vec!["song_author", "佐々木英州"]
+  );
+  assert_eq!(
+    captures_vec(&reg, ":singer       初音ミク"),
+    vec!["singer", "初音ミク"]
+  );
+  assert_eq!(
+    captures_vec(&reg, ":score_author Colk"),
+    vec!["score_author", "Colk"]
+  );
+  assert_eq!(
+    captures_vec(&reg, ":song_data    kkiminochikara-edited.wav"),
+    vec!["song_data", "kkiminochikara-edited.wav"]
+  );
+
   let reg = Regex::new(COMMENT).unwrap();
   assert!(reg.is_match("# This is a comment. "));
   assert!(reg.is_match("  # Indented!"));
@@ -52,11 +98,13 @@ impl Scoremap {
   pub fn from_file(file: std::fs::File) -> Result<(), ScoremapError> {
     use ScoremapError::*;
 
+    let property_reg = Regex::new(PROPERTY).unwrap();
     let comment_reg = Regex::new(COMMENT).unwrap();
     let command_reg = Regex::new(COMMAND).unwrap();
     let hiragana_reg = Regex::new(HIRAGANA).unwrap();
     let caption_reg = Regex::new(CAPTION).unwrap();
 
+    let mut metadata = ScoremapMetadata::new();
     let mut notes: Vec<Note> = vec![];
     let mut line_num = 1;
     let mut parsing_lyrics = false;
@@ -110,6 +158,29 @@ impl Scoremap {
         }
         let string = caption.get(0).unwrap().as_str();
         notes.push(Note::caption(line_time, string));
+      }
+      if let Some(property) = property_reg.captures(line) {
+        if parsing_lyrics {
+          return Err(InvalidPropertyDeifinition {
+            line_num,
+            reason: "プロパティの指定は歌詞定義の外のみ有効です。",
+          });
+        }
+        if property.len() != 3 {
+          return Err(InvalidPropertyDeifinition {
+            line_num,
+            reason: "プロパティの指定が正しくありません。",
+          });
+        }
+        let key = property.get(1).unwrap().as_str();
+        if !METADATA_KEYS.contains(&key) {
+          return Err(InvalidPropertyDeifinition {
+            line_num,
+            reason: "未対応のプロパティです。",
+          });
+        }
+        let value = property.get(2).unwrap().as_str();
+        metadata.insert(key.to_owned(), value.to_owned());
       }
       line_num += 1;
     }
