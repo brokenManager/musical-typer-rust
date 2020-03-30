645efc6fee0751ba121a7446901e2bb7bda86521
MikuroXina <ryosukadnak@gmail.com>
2020-03-30 11:40:37 +0900

    Impl determine_from

--- src/exp/roman_char.rs
+++ src/exp/roman_char.rs
@@ -1,24 +1,32 @@
 #[derive(Debug, Clone)]
 pub struct RomanChar {
   styles: Vec<&'static str>,
-  determined: Option<&'static str>,
 }
 
 impl RomanChar {
   pub fn new(styles: &[&'static str]) -> RomanChar {
     RomanChar {
       styles: Vec::from(styles),
-      determined: None,
     }
   }
 
-  pub fn determine_from(&mut self, input: &str) {}
+  pub fn determine_from(
+    &mut self,
+    input: &str,
+  ) -> Option<&'static str> {
+    let filtered: Vec<_> = self
+      .styles
+      .iter()
+      .filter(|style| style.starts_with(input))
+      .collect();
+    if filtered.len() != 1 {
+      None
+    } else {
+      Some(filtered[0])
+    }
+  }
 
   pub fn standard_style(&self) -> &str {
     self.styles[0]
-  }
-
-  pub fn determined(&self) -> Option<&'static str> {
-    self.determined
   }
 }
