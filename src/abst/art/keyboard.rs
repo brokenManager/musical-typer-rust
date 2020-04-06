63bf21f91f32e918e030bb5367599008532c2625
MikuroXina <ryosukadnak@gmail.com>
2020-04-06 18:32:12 +0900

    Add Keyboard

--- src/abst/art/keyboard.rs
+++ src/abst/art/keyboard.rs
@@ -1,3 +1,35 @@
 // Shows typed and pointed animation on cells of key
 
+use super::{ARTNode, ART};
+
 mod key_cell;
+
+pub struct PointEvent {
+  amount: u32,
+}
+
+pub struct KeyboardProps {
+  highlighted_keys: Vec<char>,
+  point_event: Option<PointEvent>,
+}
+
+impl Default for KeyboardProps {
+  fn default() -> Self {
+    KeyboardProps {
+      highlighted_keys: vec![],
+      point_event: None,
+    }
+  }
+}
+
+pub struct Keyboard;
+
+impl ARTNode<KeyboardProps> for Keyboard {
+  fn new(props: KeyboardProps) -> Self {
+    Keyboard
+  }
+
+  fn render(&self) -> ART {
+    unimplemented!()
+  }
+}
