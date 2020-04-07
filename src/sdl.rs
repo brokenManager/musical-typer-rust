e772f8b6c68adb900d446c1b6606b2b254f4ebce
MikuroXina <ryosukadnak@gmail.com>
2020-04-07 20:30:18 +0900

    Separate rendering to whole

--- src/sdl.rs
+++ src/sdl.rs
@@ -1,23 +1,15 @@
-extern crate sdl2;
-
-use sdl2::pixels::Color;
-use sdl2::rect::Rect;
 use sdl2::render::Canvas;
-use sdl2::video::{Window, WindowContext};
+use sdl2::video::Window;
 use sdl2::Sdl;
 
 use std::time::Duration;
-
-use crate::model::exp::sentence::Sentence;
 
 mod header;
 mod keyboard;
 mod section;
 mod text;
+mod whole;
 
-use header::Header;
-use keyboard::Keyboard;
-use section::Section;
 use text::{TextBuilder, TextError};
 
 #[derive(Debug)]
@@ -92,51 +84,6 @@ where
     })
   }
 
-  fn render<'t>(
-    &mut self,
-    builder: TextBuilder<'t, WindowContext>,
-  ) -> Result<(), ViewError> {
-    let header = Header::new("Music Name", "Composer");
-    let header_dim = Rect::new(0, 0, self.width, 100);
-
-    let to_input =
-      Sentence::new("千本桜　夜ニ紛レ", "せんぼんざくらよるにまぎれ")
-        .map_err(|e| ViewError::InitError {
-          message: format!("{:?}", e),
-        })?;
-    let section = Section::new(&to_input, 0.2);
-    let section_dim = Rect::new(0, 100, self.width, 200);
-
-    let keyboard = Keyboard::new(&['h']);
-    let keyboard_dim =
-      Rect::new(0, self.height as i32 - 300, self.width, 300);
-
-    self.canvas.set_draw_color(Color::RGB(253, 243, 226));
-    self.canvas.clear();
-
-    header.draw(&mut self.canvas, builder.clone())?;
-    self.canvas.set_draw_color(Color::RGB(0, 0, 0));
-    self
-      .canvas
-      .draw_rect(header_dim)
-      .map_err(|e| ViewError::RenderError(e))?;
-
-    section.draw(&mut self.canvas, builder.clone(), section_dim)?;
-    self.canvas.set_draw_color(Color::RGB(0, 0, 0));
-    self
-      .canvas
-      .draw_rect(section_dim)
-      .map_err(|e| ViewError::RenderError(e))?;
-
-    keyboard.draw(&mut self.canvas, builder.clone(), keyboard_dim)?;
-    self.canvas.set_draw_color(Color::RGB(0, 0, 0));
-    self
-      .canvas
-      .draw_rect(keyboard_dim)
-      .map_err(|e| ViewError::RenderError(e))?;
-    Ok(())
-  }
-
   pub fn draw(&mut self) -> Result<(), ViewError> {
     let texture_creator = self.canvas.texture_creator();
 
@@ -165,7 +112,11 @@ where
         Quit { .. } => self.controller.quit(),
         _ => {}
       }
-      self.render(builder.clone())?;
+      whole::render(
+        &mut self.canvas,
+        sdl2::rect::Rect::new(0, 0, self.width, self.height),
+        builder.clone(),
+      )?;
 
       self.canvas.present();
       ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
---
+++ src/sdl/whole.rs
@@ -0,0 +1,55 @@
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::{Window, WindowContext};
+
+use crate::model::exp::sentence::Sentence;
+
+use super::header::Header;
+use super::keyboard::Keyboard;
+use super::section::Section;
+use super::text::TextBuilder;
+use super::ViewError;
+
+pub fn render<'t>(
+  mut canvas: &mut Canvas<Window>,
+  client: Rect,
+  builder: TextBuilder<'t, WindowContext>,
+) -> Result<(), ViewError> {
+  let header = Header::new("Music Name", "Composer");
+  let header_dim = Rect::new(0, 0, client.width(), 100);
+
+  let to_input =
+    Sentence::new("千本桜　夜ニ紛レ", "せんぼんざくらよるにまぎれ")
+      .map_err(|e| ViewError::InitError {
+      message: format!("{:?}", e),
+    })?;
+  let section = Section::new(&to_input, 0.2);
+  let section_dim = Rect::new(0, 100, client.width(), 200);
+
+  let keyboard = Keyboard::new(&['h']);
+  let keyboard_dim =
+    Rect::new(0, client.height() as i32 - 300, client.width(), 300);
+
+  canvas.set_draw_color(Color::RGB(253, 243, 226));
+  canvas.clear();
+
+  header.draw(&mut canvas, builder.clone())?;
+  canvas.set_draw_color(Color::RGB(0, 0, 0));
+  canvas
+    .draw_rect(header_dim)
+    .map_err(|e| ViewError::RenderError(e))?;
+
+  section.draw(&mut canvas, builder.clone(), section_dim)?;
+  canvas.set_draw_color(Color::RGB(0, 0, 0));
+  canvas
+    .draw_rect(section_dim)
+    .map_err(|e| ViewError::RenderError(e))?;
+
+  keyboard.draw(&mut canvas, builder.clone(), keyboard_dim)?;
+  canvas.set_draw_color(Color::RGB(0, 0, 0));
+  canvas
+    .draw_rect(keyboard_dim)
+    .map_err(|e| ViewError::RenderError(e))?;
+  Ok(())
+}
