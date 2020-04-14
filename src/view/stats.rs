a5a04b117368c0cbe3200eb28f7d987bae1487a0
MikuroXina <ryosukadnak@gmail.com>
2020-04-14 15:08:55 +0900

    Add accuracy and achievement rate to stats

--- src/view/stats.rs
+++ src/view/stats.rs
@@ -26,21 +26,55 @@ pub fn render<'t>(
     .build()?
     .render(
       &mut canvas,
-      Rect::new(client.x() + 10, client.y() + 10, 70, 10),
+      Rect::new(client.x() + 10, client.y() + 10, 90, 20),
     )?;
-  builder.text("正解率").build()?.render(
-    &mut canvas,
-    Rect::new(client.x() + 10, client.y() + 30, 30, 10),
-  )?;
-  builder.text("達成率").build()?.render(
-    &mut canvas,
-    Rect::new(
-      client.width() as i32 / 2 + client.x() + 10,
-      client.y() + 30,
-      30,
-      10,
-    ),
-  )?;
+  builder
+    .text("正解率")
+    .color(Color::RGB(160, 160, 165))
+    .build()?
+    .render(
+      &mut canvas,
+      Rect::new(client.x() + 10, client.y() + 30, 50, 20),
+    )?;
+  builder
+    .text(&format!("{:05.1}%", props.accuracy))
+    .color(Color::RGB(0, 0, 0))
+    .build()?
+    .render(
+      &mut canvas,
+      Rect::new(
+        client.x() + 60,
+        client.y() + 30,
+        ((client.width() / 2) as i32 - (client.x() + 60)) as u32,
+        client.height() - 20,
+      ),
+    )?;
+  builder
+    .text("達成率")
+    .color(Color::RGB(160, 160, 165))
+    .build()?
+    .render(
+      &mut canvas,
+      Rect::new(
+        client.width() as i32 / 2 + client.x() + 10,
+        client.y() + 30,
+        50,
+        20,
+      ),
+    )?;
+  builder
+    .text(&format!("{:05.1}%", props.achievement_rate))
+    .color(Color::RGB(64, 79, 181))
+    .build()?
+    .render(
+      &mut canvas,
+      Rect::new(
+        (client.width() / 2) as i32 + client.x() + 60,
+        client.y() + 30,
+        ((client.width() / 2) as i32 - (client.x() + 60)) as u32,
+        client.height() - 20,
+      ),
+    )?;
   let rank = rank::rank(props.accuracy);
   Ok(())
 }
