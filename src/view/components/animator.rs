404dbd47433bf5a7644dc380b85507fa57ac4d84
MikuroXina <ryosukadnak@gmail.com>
2020-07-14 11:30:01 +0900

    Add TransparentComponent

--- src/view/components/animator.rs
+++ src/view/components/animator.rs
@@ -1,22 +1,45 @@
 use crate::{
   model::exp::time::Seconds,
-  view::renderer::{text::TextStyle, RenderCtx, ViewResult},
+  view::renderer::{Component, RenderCtx, ViewResult},
 };
 
-pub fn fade_out_text(
-  duration: Seconds,
-  styler: impl Fn(TextStyle) -> TextStyle,
-) -> impl FnMut(RenderCtx, Seconds) -> ViewResult {
-  let mut time: Seconds = 0.0.into();
-  move |ctx: RenderCtx, delta_time| -> ViewResult {
-    time += delta_time;
-    let ratio = time.clone() / duration.clone();
-    let opacity =
-      (u8::max_value() as f64 * (1.0 - ratio).max(0.0)) as u8;
+mod fade_out_text;
 
-    ctx
-      .borrow_mut()
-      .text(|style: TextStyle| styler(style).opacity(opacity))?;
-    Ok(())
+pub use fade_out_text::FadeOutText;
+
+pub trait TransparentComponent: Component {
+  fn render(
+    &self,
+    ctx: RenderCtx<'_, '_>,
+    opacity: f64,
+  ) -> ViewResult;
+}
+
+pub struct FadeOutProps<ChildProp> {
+  time: Seconds,
+  child: Box<dyn TransparentComponent<Props = ChildProp>>,
+}
+
+pub struct FadeOut<ChildProp> {
+  props: FadeOutProps<ChildProp>,
+  duration: Seconds,
+}
+
+impl Component for FadeOut {
+  type Props = FadeOutProps;
+
+  fn is_needed_redraw(&self, new_props: &Self::Props) -> bool {
+    todo!()
+  }
+
+  fn update(&mut self, new_props: Self::Props) {
+    self.props = new_props;
+  }
+
+  fn render(&self, ctx: RenderCtx<'_, '_>) -> ViewResult {
+    let ratio = self.props.time.clone() / self.duration.clone();
+    let opacity = (1.0 - ratio).max(0.0);
+
+    self.props.child.render(ctx, opacity);
   }
 }
