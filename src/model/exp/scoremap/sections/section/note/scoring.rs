a7c68ea56901606f662c0853db106b6c2dcd8821
MikuroXina <ryosukadnak@gmail.com>
2021-05-26 23:49:59 +0900

    refactor: Remove extra modules

--- src/model/exp/scoremap/sections/section/note/scoring.rs
+++ src/model/exp/scoremap/sections/section/note/scoring.rs
@@ -1,10 +1,40 @@
 use super::super::note::TypeResult;
 
-mod accuracy;
-mod achievement_rate;
+#[derive(Debug, Default, Clone)]
+pub struct Accuracy {
+  correction_type_count: u32,
+  wrong_type_count: u32,
+}
 
-use accuracy::Accuracy;
-use achievement_rate::AchievementRate;
+impl Accuracy {
+  fn point(&mut self, res: &TypeResult) {
+    if let TypeResult::Succeed = res {
+      self.correction_type_count += 1;
+    } else if let TypeResult::Mistaken = res {
+      self.wrong_type_count += 1;
+    }
+  }
+
+  pub fn as_f64(&self) -> f64 {
+    if self.correction_type_count + self.wrong_type_count == 0 {
+      return 0.0;
+    }
+    self.correction_type_count as f64
+      / (self.correction_type_count + self.wrong_type_count) as f64
+  }
+}
+
+impl From<Accuracy> for f64 {
+  fn from(acc: Accuracy) -> Self {
+    acc.as_f64()
+  }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct AchievementRate {
+  correction_type_count: u32,
+  whole_type_count: u32,
+}
 
 #[derive(Debug, Default, Clone)]
 pub struct Scoring {
